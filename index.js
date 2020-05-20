9c3138c8a3624b6b1c704459bda6a502898ba2ee
darnocer <darnocer@gmail.com>
2020-05-20 16:55:36 -0600

    inquirer

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-const questions = [
+var inquirer = require("inquirer");
 
-];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Project title:",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Project description:",
+      name: "password",
+    },
+    {
+      type: "input",
+      message: "Installation:",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Usage:",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "License:",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "Contributing:",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Tests:",
+      name: "tests",
+    },
+  ])
+  .then(function (response) {
+    console.log(`Title: ${response.title}`);
+  });
 
-function writeToFile(fileName, data) {
-}
+// const questions = [
 
-function init() {
+// ];
 
-}
+// function writeToFile(fileName, data) {
+// }
 
-init();
+// function init() {
+
+// }
+
+// init();
